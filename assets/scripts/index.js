937d646e8578cfe2376e35c521cc20ba0609fbd0
MaksimDimov <maksymdimov@student.mitt.ca>
2023-12-16 12:13:42 +0200

    looks like fully done

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,27 +1,42 @@
 'use strict';
 
-// $.ajax({
-//     url: 'https://randomuser.me/api/',
-//     dataType: 'json',
-//     success: function(data) {
-//       console.log(data);
-//     }
-//   });
+const peopleMayKnow = document.querySelector('.people-you-may-know');
 
-  async function getRandomUsers(numUsers) {
-    const apiUrl = `https://randomuser.me/api/?results=${numUsers}`;
-    
-    try {
-      const response = await fetch(apiUrl);
-      const data = await response.json();
-      const users = data.results;
-      console.log(users);
-  
-      return users;
-    } catch (error) {
-      console.error('Error fetching random users:', error);
-      return null;
-    }
-  }
-  
-  getRandomUsers(10);
+fetch('https://randomuser.me/api/?results=10')
+.then(response => response.json())
+.then(data => {
+    const users = data.results;
+
+    users.forEach(user => {
+        const userCard = document.createElement('div');
+        userCard.classList.add('people-you-may-know-card');
+
+        const userImg = document.createElement('img');
+        userImg.src = user.picture.medium;
+        userImg.alt = 'User Profile Picture';
+        userImg.classList.add('people-you-may-know-img');
+
+        const userName = document.createElement('p');
+        userName.classList.add('people-you-may-know-name');
+        userName.textContent = `${user.name.first} ${user.name.last}`;
+
+        const userCity = document.createElement('p');
+        userCity.classList.add('people-you-may-know-city');
+        userCity.textContent = user.location.city;
+
+        const textContentPeopleMayKnow = document.createElement('div');
+        textContentPeopleMayKnow.classList.add('people-you-may-know-text-content');
+
+        const addFriend = document.createElement('i');
+        addFriend.classList.add('fa-solid', 'fa-plus', 'people-you-may-know-add-friend');
+                    
+        textContentPeopleMayKnow.appendChild(userName);
+        textContentPeopleMayKnow.appendChild(userCity);
+
+        userCard.appendChild(userImg);
+        userCard.appendChild(textContentPeopleMayKnow);
+        peopleMayKnow.appendChild(userCard);
+        userCard.appendChild(addFriend);
+    });
+})
+.catch(error => console.error('Error fetching data:', error));
